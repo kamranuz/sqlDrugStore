72200dd1a1f81a4dbceefc19dd03b87f1c7c7c66
OMALOE <56366333+OMALOE@users.noreply.github.com>
2021-06-11 22:13:10 +0300

    Update JOIN_triple.sql

--- SELECTS/JOIN_triple.sql
+++ SELECTS/JOIN_triple.sql
@@ -1,1 +1,5 @@
-select * from Product p inner join (select pd.product_id, pd.name as pname, md.name as mname from ProductDict pd inner join ManufacturerDict md on md.manufacturer_id=pd.manufacturer_id) sel on sel.product_id=p.product_id;
+--выручка по отдельному товару
+select pd.name, SUM(psp.sellPrice) from Product p 
+inner join ProductDict pd on pd.product_id=p.product_id and p.productStatus_id=3 
+inner join ProductSellPrice psp on psp.barcode=p.barcode 
+GROUP BY pd.name;
