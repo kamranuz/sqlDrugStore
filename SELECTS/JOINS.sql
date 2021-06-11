818a57a7238f4761983eca5eccbfdac8d1d4583b
OMALOE <56366333+OMALOE@users.noreply.github.com>
2021-06-11 22:12:08 +0300

    Update JOINS.sql

--- SELECTS/JOINS.sql
+++ SELECTS/JOINS.sql
@@ -1,3 +1,8 @@
-select p.*, pd.name from Product p inner join ProductDict pd on p.product_id=pd.product_id;
-select md.*, cd.name from ManufacturerDict md inner join CountryDict cd on cd.country_id=md.country_id;
-select sd.*, cd.name from SupplierDict sd inner join CityDict cd on cd.city_id=cd.city_id;
+--количество поставщиков по странам
+select cd.name, COUNT(md.manufacturer_id) as amount from CountryDict cd inner join ManufacturerDict md on md.country_id=cd.country_id GROUP by cd.name; 
+
+--количество накладных по поставщикам
+select sd.name, COUNT(ih.invoice_id) from SupplierDict sd inner join InvoiceHeader ih on ih.supplier_id=sd.supplier_id GROUP BY sd.name;
+
+--позиции по количеству в заказе
+select pd.name, SUM(od.qty) as 'total ordered' from OrderDetail od inner join ProductDict pd on pd.product_id=od.product_id GROUP BY pd.name;
