a6d9502365669ed42fb7d928a61af2ca5e612777
kamranuz <78980405+kamranuz@users.noreply.github.com>
2021-06-14 19:49:48 +0300

    Update selects.sql

--- toSumbit/selects.sql
+++ toSumbit/selects.sql
@@ -1,1 +1,15 @@
+-- lag query
+select barcode, name, prev, sellPrice, isHIgher from (
+select pbp.barcode, pbp.finish , p.product_id, pd.name, 
+	lag(pbp.sellPrice) over (ORDER by pbp.start) as prev,
+	pbp.sellPrice, isHigher =
+		CASE
+			when pbp.sellPrice > lag(pbp.sellPrice) over (ORDER by pbp.start) then 'higher'
+			when pbp.sellPrice < lag(pbp.sellPrice) over (ORDER by pbp.start) then 'lower'
+			else 'Prev price is null'
+		END 
 
+from ProductBuyPrice pbp
+	inner join Product p on p.barcode=pbp.barcode
+	inner join ProductDict pd on pd.product_id=p.product_id
+) as a where a.finish is null;
